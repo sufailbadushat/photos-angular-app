4075bb0bdf57db63b3025cf421dec0564b165d81
sufailbadushat <sufailbadushat@gamil.com>
2022-11-29 15:15:06 +0530

    view static data add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ViewPhotoComponent } from './view-photo/view-photo.component';
 
 const myRoute:Routes=[
   {
     path:"",
     component:NavBarComponent
+  },
+  {
+    path:"view",
+    component:ViewPhotoComponent
   }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ViewPhotoComponent
   ],
   imports: [
     BrowserModule,
